a5a68fb949ed29142b4c8709f789ea436e221825
Alex Knight <alex@aktheknight.co.uk>
2025-02-11 21:09:03 +0000

    Add attribute usage to MessageProcessorAttribute

--- source/IrcDotNet/Attributes/MessageProcessorAttribute.cs
+++ source/IrcDotNet/Attributes/MessageProcessorAttribute.cs
@@ -5,6 +5,7 @@ namespace IrcDotNet.Attributes;
 
 // Indicates that method processes message for some protocol.
 [MeansImplicitUse]
+[AttributeUsage(AttributeTargets.Method)]
 internal class MessageProcessorAttribute : Attribute
 {
     public MessageProcessorAttribute(string commandName)
